4d7210e983e8e994e7044aa6ef30f0f9f89e9d69
Tyler Kirane <tkirane@gmail.com>
2015-02-10 16:41:52 -0500

    added link to Craigslist

---
+++ Cogsy/CLWebView.swift
@@ -0,0 +1,21 @@
+//
+//  CLWebView.swift
+//  Cogsy
+//
+//  Created by Tyler Kirane on 2/10/15.
+//  Copyright (c) 2015 enarik. All rights reserved.
+//
+
+import UIKit
+
+class CLWebView: UIWebView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Cogsy/CraigslistViewController.swift
@@ -0,0 +1,41 @@
+//
+//  CraigslistViewController.swift
+//  Cogsy
+//
+//  Created by Tyler Kirane on 2/10/15.
+//  Copyright (c) 2015 enarik. All rights reserved.
+//
+
+import UIKit
+
+class CraigslistViewController: UIViewController {
+    
+    
+    @IBOutlet weak var clView: CLWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let requestURL = NSURL(string: "http://craigslist.org")
+        let request = NSURLRequest(URL: requestURL!)
+        clView.loadRequest(request)
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+}
+
+
+/*
+// MARK: - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+// Get the new view controller using segue.destinationViewController.
+// Pass the selected object to the new view controller.
+}
+*/
+
+}
