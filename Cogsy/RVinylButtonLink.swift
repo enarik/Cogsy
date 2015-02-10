3620003baca3fd502440c44bf96e15e0f9339d45
Tyler Kirane <tkirane@gmail.com>
2015-02-10 14:46:12 -0500

    added link to r/vinyl

---
+++ Cogsy/RVinylButtonLink.swift
@@ -0,0 +1,21 @@
+//
+//  RVinylButtonLink.swift
+//  Cogsy
+//
+//  Created by Tyler Kirane on 2/10/15.
+//  Copyright (c) 2015 enarik. All rights reserved.
+//
+
+import UIKit
+
+class RVinylButtonLink: UIWebView {
+    
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Cogsy/RVinylViewController.swift
@@ -0,0 +1,41 @@
+//
+//  RVinylViewController.swift
+//  Cogsy
+//
+//  Created by Tyler Kirane on 2/10/15.
+//  Copyright (c) 2015 enarik. All rights reserved.
+//
+
+import UIKit
+
+class RVinylViewController: UIViewController {
+    
+        
+    @IBOutlet weak var vinylView: RVinylButtonLink!
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            let requestURL = NSURL(string: "http://reddit.com/r/vinyl")
+            let request = NSURLRequest(URL: requestURL!)
+            vinylView.loadRequest(request)
+            
+            // Do any additional setup after loading the view.
+        }
+        
+        override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+            // Dispose of any resources that can be recreated.
+        }
+        
+        
+        /*
+        // MARK: - Navigation
+        
+        // In a storyboard-based application, you will often want to do a little preparation before navigation
+        override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+        }
+        */
+        
+}
