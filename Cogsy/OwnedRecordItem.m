e7ec2b4236c0e0c7ad6511cecdb70fb1393da335
Tyler Kirane <tkirane@gmail.com>
2015-02-12 12:45:36 -0500

    First commit, Objective C and Swift integration

---
+++ Cogsy/OwnedRecordItem.m
@@ -0,0 +1,13 @@
+//
+//  OwnedRecordItem.m
+//  Cogsy
+//
+//  Created by Tyler Kirane on 2/12/15.
+//  Copyright (c) 2015 enarik. All rights reserved.
+//
+
+#import "OwnedRecordItem.h"
+
+@implementation OwnedRecordItem
+
+@end
