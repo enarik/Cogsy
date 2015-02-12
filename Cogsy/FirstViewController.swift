a9e8d5c8e3ae4759ffbf7f6c60ab57a621e9e13c
Tyler Kirane <tkirane@gmail.com>
2015-02-12 15:07:15 -0500

    added semi functional tableview

--- Cogsy/FirstViewController.swift
+++ Cogsy/FirstViewController.swift
@@ -8,13 +8,67 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var ownedTableView: UITableView!
+    
+    var ownedRecords = [String]()
+    
+    @IBAction func addOwnedRecord(sender: AnyObject) {
+        var alert = UIAlertController(title: "Record",
+            message: "Add a record you own",
+            preferredStyle: .Alert)
+        
+        let saveAction = UIAlertAction(title: "Save",
+            style: .Default) { (action: UIAlertAction!) -> Void in
+                
+                let textField = alert.textFields![0] as UITextField
+                self.ownedRecords.append(textField.text)
+                self.ownedTableView.reloadData()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+            style: .Default) { (action: UIAlertAction!) -> Void in
+        }
+        
+        alert.addTextFieldWithConfigurationHandler {
+            (textField: UITextField!) -> Void in
+        }
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        presentViewController(alert,
+            animated: true,
+            completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        title = "Owned Records"
+        ownedTableView.registerClass(UITableViewCell.self,
+            forCellReuseIdentifier: "Cell")
     }
 
+    // MARK: UITableViewDataSource
+    func tableView(tableView: UITableView,
+        numberOfRowsInSection section: Int) -> Int {
+            return ownedRecords.count
+    }
+    
+    func tableView(tableView: UITableView,
+        cellForRowAtIndexPath
+        indexPath: NSIndexPath) -> UITableViewCell {
+            
+            let cell =
+            ownedTableView.dequeueReusableCellWithIdentifier("Cell")
+                as UITableViewCell
+            
+            cell.textLabel!.text = ownedRecords[indexPath.row]
+            
+            return cell
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
